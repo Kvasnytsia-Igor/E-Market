098ad3734096050027917967eaa87da3cfd78a7e
kvass-gif <igorkvasnytsia94@gmail.com>
2023-06-26 12:14:04 +0300

    Js

---
+++ html-css/Table/table.js
@@ -0,0 +1,3 @@
+export const sayHello = () => {
+    document.getElementById("content").innerHTML = "Hello world";
+};
---
+++ html-css/scripts.js
@@ -0,0 +1,7 @@
+import { sayHello } from "./Table/table.js";
+
+function main() {
+  //sayHello();
+}
+
+main();
